339382b60d60e24a2bd045036176cf412daa70b0
Fernando <deluk231lukkah@gmail.com>
2020-06-23 11:06:42 -0300

    Creación de la clase barrio

---
+++ src/main/java/ar/edu/unju/fi/trackpersonas/model/Barrio.java
@@ -0,0 +1,40 @@
+package ar.edu.unju.fi.trackpersonas.model;
+
+/**
+ * Clase Barrio
+ * @author Fernando
+ *
+ */
+public class Barrio {
+	//Variables miembro
+	String nombre;
+	
+	
+	//Constructores---------------------------------------------------------------
+	
+	/**
+	 * Constructor por defecto
+	 */
+	public Barrio() {
+	}
+
+
+	//Métodos accesores-----------------------------------------------------------
+	/**
+	 * Obtiene el valor de la variable miembro nombre
+	 * @return nombre
+	 */
+	public String getNombre() {
+		return nombre;
+	}
+
+	/**
+	 * Asigna un nombre a la variable miembro nombre
+	 * @param nombre
+	 */
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	
+	
+}
